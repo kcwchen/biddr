316b905283b8f95b64c3a57df0ace70118df777e
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 11:26:48 -0800

    Add session routes

--- biddr_api/config/routes.rb
+++ biddr_api/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :auctions do
         resources :bids, only: [:create]
       end
+      resource :session, only: [:create, :destroy]
     end
   end
 end
