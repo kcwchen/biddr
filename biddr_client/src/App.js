8d3257a7b2f883ce4c95a5aa03229e31808f847f
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 12:03:17 -0800

    Add react-router-dom and WelcomePage component

--- biddr_client/src/App.js
+++ biddr_client/src/App.js
@@ -1,8 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={WelcomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ biddr_client/src/components/WelcomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>
+        Going twice. Going once. <br /> Sold to Biddr!
+      </h1>
+    </div>
+  );
+};
+
+export default WelcomePage;
