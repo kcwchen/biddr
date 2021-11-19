44eba1f95ae246b900821dba700b626bcb5b524d
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 10:30:54 -0800

    Add api/v1/auctions routes

--- biddr_api/app/models/auction.rb
+++ biddr_api/app/models/auction.rb
@@ -1,5 +1,6 @@
 class Auction < ApplicationRecord
-
+  has_many :bids, dependent: :destroy
+  
   validates :title, presence: true, length: {minimum: 1}
   validates :reserve_price, numericality: {greater_than_or_equal_to: 0}
 end
