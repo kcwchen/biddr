e9f68ab5cf3488b41eb40a494ea0c970bd6c639f
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 10:41:45 -0800

    Add seed file and index action of auctions

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::AuctionsController < Api::ApplicationController
+
+  def index
+    auctions = Auction.order(created_at: :desc)
+    render(json: auctions)
+  end
 end
--- biddr_api/db/seeds.rb
+++ biddr_api/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Bid.destroy_all
+Auction.destroy_all
+
+20.times do
+  created_at = Faker::Date.backward(days: 50)
+
+  a = Auction.create(
+    title: Faker::Vehicle.make_and_model,
+    description: Faker::Vehicle.standard_specs.join(', '),
+    ends_at: Faker::Date.forward(days: 50),
+    reserve_price: Faker::Commerce.price,
+    created_at: created_at,
+    updated_at: created_at
+  )
+end
+
+auctions = Auction.all
+puts auctions.count
