6508ab0795359d3b13762c66dd0a5dc35fa6f516
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 11:00:31 -0800

    Add is_admin to users and update seed file

--- biddr_api/db/seeds.rb
+++ biddr_api/db/seeds.rb
@@ -6,8 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 Bid.destroy_all
 Auction.destroy_all
+
+PASSWORD = '123'
+
+super_user = User.create(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin@user.com",
+  password: PASSWORD,
+  is_admin: true
+)
+
+5.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  User.create(
+    first_name: first_name,
+    last_name: last_name,
+    email: "#{first_name.downcase}@#{last_name.downcase}.com",
+    password: PASSWORD
+  )
+end
+
+users = User.all
 
 20.times do
   created_at = Faker::Date.backward(days: 50)
@@ -18,9 +42,11 @@ Auction.destroy_all
     ends_at: Faker::Date.forward(days: 50),
     reserve_price: Faker::Commerce.price,
     created_at: created_at,
-    updated_at: created_at
+    updated_at: created_at,
+    user: users.sample
   )
 end
 
 auctions = Auction.all
+puts users.count
 puts auctions.count
