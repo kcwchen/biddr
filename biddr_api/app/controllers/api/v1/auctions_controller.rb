0ecc2b945afc64e03f4584e2ebb40abd9db6e55e
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 10:44:57 -0800

    Add show action for auctions controller

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::AuctionsController < Api::ApplicationController
+  before_action :find_auction, except: [:index, :create]
 
   def index
     auctions = Auction.order(created_at: :desc)
     render(json: auctions)
+  end
+
+  def show
+    if @auction
+      render(json: @auction)
+    else
+      render(json: {error: "Auction not found"})
+    end
+  end
+
+  private
+
+  def find_auction
+    @auction = Auction.find params[:id]
   end
 end
