eed3713076d47fd18d174ccf8ec0e72ef37c5a9b
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 10:49:16 -0800

    Add create action for auctions controller

--- biddr_api/app/controllers/api/v1/auctions_controller.rb
+++ biddr_api/app/controllers/api/v1/auctions_controller.rb
@@ -14,9 +14,22 @@ class Api::V1::AuctionsController < Api::ApplicationController
     end
   end
 
+  def create
+    auction = Auction.new(auction_params)
+    if auction.save
+      render(json: {id: auction.id})
+    else
+      render(json: {error: auction.errors, status: 422})
+    end
+  end
+
   private
 
   def find_auction
     @auction = Auction.find params[:id]
+  end
+
+  def auction_params
+    params.require(:auction).permit(:title, :description, :ends_at, :reserve_price)
   end
 end
