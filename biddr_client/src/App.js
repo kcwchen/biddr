1faca24ad1cdd19f3fb20c3f04a13503b3dcd0df
Kevin Chen <chen.kcw@gmail.com>
2021-11-19 13:26:01 -0800

    Sign in page

--- biddr_client/src/App.js
+++ biddr_client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { User } from './requests';
 import NavBar from './components/NavBar';
 import WelcomePage from './components/WelcomePage';
 import AuctionIndexPage from './components/AuctionIndexPage';
@@ -7,6 +9,20 @@ import AuctionShowPage from './components/AuctionShowPage';
 import SignInPage from './components/SignInPage';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getCurrentUser();
+  }, []);
+
+  const getCurrentUser = () => {
+    return User.current().then((user) => {
+      if (user?.id) {
+        setUser(user);
+      }
+    });
+  };
+
   return (
     <BrowserRouter>
       <NavBar />
--- biddr_client/src/requests.js
+++ biddr_client/src/requests.js
@@ -30,3 +30,21 @@ export const Session = {
     }).then((res) => res.json());
   },
 };
+
+export const User = {
+  current() {
+    return fetch(`${baseUrl}/users/current`, {
+      credentials: 'include',
+    }).then((res) => res.json());
+  },
+  create(params) {
+    return fetch(`${baseUrl}/users`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: params }),
+    }).then((res) => res.json());
+  },
+};
