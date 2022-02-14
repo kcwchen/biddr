b3a9d76be7a48ef7d554d3e9d6eebe2f2eeca8b7
Kevin Chen <chen.kcw@gmail.com>
2022-02-14 03:54:19 -0800

    make changes for deploy

--- biddr_api/config/application.rb
+++ biddr_api/config/application.rb
@@ -34,5 +34,11 @@ module BiddrApi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Specify cookies SameSite protection level: either :none, :lax, or :strict.
+    # This change is not backwards compatible with earlier Rails versions. 
+    # It's best enabled when your entire app is migrated and stable on 6.1.
+    # Was not in Rails 6.0. Default in rails 6.1 is :lax, not :strict
+    config.action_dispatch.cookies_same_site_protection = :none
   end
 end
